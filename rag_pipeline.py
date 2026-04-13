2f4587de45188d1da37088e8418e2cc28bfc16a7
seemareddy <seemareddychapala27@gmail.com>
2026-04-13 17:54:28 +0530

    Fixed text splitter import for deployment

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 from langchain_community.document_loaders import WebBaseLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
